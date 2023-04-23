d7d19d26d16c6f224986f3966e96f8804e83d0ee
caamillo <mcamillolud@gmail.com>
2023-04-23 23:32:41 +0200

    Edits

--- index.php
+++ index.php
@@ -5,9 +5,39 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link rel="stylesheet" href="output.css">
 </head>
 <body>
-    <table id="toscana"></table>
+    <div class="p-5 space-y-3">
+        <div class="flex justify-start items-center space-x-5">
+            <img src="lucca.png" class="w-[50px]" />
+            <h1 class="text-[25px] font-bold">Istituzioni non profit della provincia di Lucca</h1>
+        </div>
+        <h3 class="w-[50%] py-3">Dati primari: numero di istituzioni non profit attive e relativi addetti, lavoratori esterni e volontari, con sede legale in provincia di Lucca classificate per sezioni e divisioni ATECO 2007. Confronto fra censimenti Data di riferimento: 31 dicembre 2011. Fonte: ISTAT, Censimento delle Istituzioni Non Profit. Estrazione dei dati: a cura dell'Ufficio di Statistica della Provincia di Lucca, dalla banca dati i.stat. Eventuali errori sono a carico di chi ha effettuato l'estrazione e non devono essere imputati all'ISTAT. Dati aggiuntivi: variazioni nel numero di imprese, di addetti, di lavoratori esterni e di volontari Metadati e informazioni aggiuntive: a cura dell'Ufficio di Statistica della Provincia di Lucca.</h3>
+        <button onClick="toggleTable()" class="px-4 py-3 transition-all hover:scale-95 hover:opacity-80 font-medium rounded-md bg-sky-600 text-white">Show Table</button>
+    </div>
+    <div id="blur" class="w-full h-full bg-[#00000030] duration-700 transition-all blur-off backdrop-blur-sm fixed top-0"></div>
+    <div id="box" class="fixed w-[80%] h-[80%] transition-all duration-500 closed bg-white p-5 rounded-lg shadow-2xl top-1/2 translate-y-[-50%] left-1/2 translate-x-[-50%] overflow-y-scroll">
+        <table id="toscana">
+            <tr>
+                <td>Sezioni</td>
+                <td>Codice</td>
+                <td>Istuzioni Att. 2001</td>
+                <td>Istuzioni Att. 2011</td>
+                <td>Variazione Istruzioni</td>
+                <td>Addetti 2001</td>
+                <td>Addetti 2011</td>
+                <td>Variazione Addetti</td>
+                <td>Lavoratori Esterni 2001</td>
+                <td>Lavoratori Esterni 2011</td>
+                <td>Variazione Lavoratori Esterni</td>
+                <td>Volontari 2001</td>
+                <td>Volontari 2011</td>
+                <td>Variazione Volontari</td>
+            </tr>
+        </table>
+        <button onClick="toggleTable()" class="bg-red-500 px-6 py-3 mt-5 transition-all hover:scale-95 hover:opacity-80 font-medium rounded-md text-white">Close Table</button>
+    </div>
     <script src="./script.js" type="text/javascript"></script>
 </body>
 </html>
--- script.js
+++ script.js
@@ -7,10 +7,17 @@ fetch('/include/toscana.php')
         const table = document.getElementById('toscana')
         console.log(data)
         for (let row in data) {
-            const currRow = table.insertRow(row)
+            const currRow = table.insertRow(parseInt(row) + 1)
             for (let col in Object.keys(data[row])) {
                 const currCel = currRow.insertCell(col)
                 currCel.innerHTML = data[row][Object.keys(data[row])[col]]
             }
         }
     })
+
+const toggleTable = () => {
+    const table = document.getElementById('box')
+    const blur = document.getElementById('blur')
+    table.classList.toggle('closed')
+    blur.classList.toggle('blur-off')
+}
